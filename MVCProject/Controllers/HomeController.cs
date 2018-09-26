81a6f1aaeef0a29b330edf05ddd78b46aadf0977
Cavel Vellenoweth <Cavelvellenoweth@hotmail.co.uk>
2018-09-26 14:13:30 +0100

    PayPal Functionality added
    Basic paypal functions added final version can be made once cart is finished

--- MVCProject/Controllers/HomeController.cs
+++ MVCProject/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using MVCProject.Context;
 using MVCProject.Models;
+using PayPal.Api;
+
 namespace MVCProject.Controllers
 {
     public class HomeController : Controller
@@ -42,6 +44,91 @@ namespace MVCProject.Controllers
             ViewBag.Products = db.Products.ToList();
             ViewData["Products"] = db.Products.ToList();
             return View(db.Categories.ToList());
+        }
+        public ActionResult Payment()
+        {
+            var apiContext = GetApiContext();
+            var payment = new Payment
+            {
+                intent = "sale",
+                payer = new Payer
+                {
+                    payment_method = "paypal"
+                },
+                transactions = new List<Transaction>
+                {
+                    new Transaction
+                    {
+                        description = "You bought an animal",
+                        amount = new Amount
+                        {
+                            currency = "GBP",
+                            total = 99.99M.ToString(),
+                        },
+                        item_list = new ItemList()
+                        {
+                            items = new List<Item>()
+                            {
+                                new Item()
+                                {
+                                    description = "You bought an animal",
+                                    currency = "GBP",
+                                    price = 99.99M.ToString(),
+                                    quantity = "1",
+                                }
+                            }
+                        }
+                    }
+                },
+                redirect_urls = new RedirectUrls
+                {
+                    return_url = Url.Action("Return", "Home", null, Request.Url.Scheme),
+                    cancel_url = Url.Action("Cancel", "Single", null, Request.Url.Scheme),
+                }
+            };
+
+            //send payment to paypal
+            var createdPayment = payment.Create(apiContext);
+
+            //save reference to the paypal payment
+
+            //find the approval url to send our user to
+            var approvalUrl =
+                createdPayment.links.FirstOrDefault(
+                    x => x.rel.Equals("approval_url",
+                    StringComparison.OrdinalIgnoreCase));
+
+            //send the user to paypal to approve payment
+            return Redirect(approvalUrl.href);
+        }
+        public ActionResult Return(string payerId, string paymentId)
+        {
+            var apiContext = GetApiContext();
+
+            //set payer for payment
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = payerId
+            };
+
+            //identify the payment to execute
+            var payment = new Payment()
+            {
+                id = paymentId
+            };
+            var executedpayement = payment.Execute(apiContext, paymentExecution);
+            return RedirectToAction("Index");
+        }
+
+        //return View(db.Categories.ToList());
+        private APIContext GetApiContext()
+        {
+            //authenticate with paypal
+            var config = ConfigManager.Instance.GetProperties();
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+            return apiContext;
+
         }
     }
 }
